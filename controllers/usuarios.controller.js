2564218a259380c3e4be4bb45afcf2bd16fb7209
Hazael Lopez <elhazael134@gmail.com>
2022-07-10 08:58:10 -0600

    Parámetros de segmento y query

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,9 +1,17 @@
 const { response } = require("express")
 
 const usuariosGet = (req, res = response) => {
+
+    const { q, nombre = 'No name', apikey, page = 1, limit = 10 } = req.query;
+
     res.json({
-        msg: 'Get Api - Controlador'
-    })
+        msg: 'Get Api - Controlador',
+        q,
+        nombre,
+        apikey, 
+        page, 
+        limit
+    });
 }
 
 const usuariosPost = (req, res) => {
@@ -14,25 +22,32 @@ const usuariosPost = (req, res) => {
         msg: 'Post Api - Controlador',
         nombre,
         edad
-    })
+    });
 }
 
 const usuariosPut = (req, res) => {
+
+    const { id } = req.params;
+
     res.json({
-        msg: 'Put Api - Controlador'
-    })
+        msg: 'Put Api - Controlador',
+        id
+    });
 }
 
 const usuariosDelete = (req, res) => {
+
+    const { id } = req.params;
+
     res.json({
         msg: 'Delete Api - Controlador'
-    })
+    });
 }
 
 const usuariosPatch = (req, res) => {
     res.json({
         msg: 'Patch Api - Controlador'
-    })
+    });
 }
 
 module.exports = {
